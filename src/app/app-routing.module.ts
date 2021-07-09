028219fb4a83b86e0bbe9663052181293a087a65
aifqb <alexquisbert@gmail.com>
2021-07-09 17:55:50 -0400

    con paginas con rutas completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from "@angular/core";
 import { Router, RouterModule } from "@angular/router";
+import { ContactoComponent } from "./pages/contacto/contacto.component";
+import { EncuentranosComponent } from "./pages/encuentranos/encuentranos.component";
+import { EventosComponent } from "./pages/eventos/eventos.component";
+import { InicioComponent } from "./pages/inicio/inicio.component";
 import { ItemComponent } from "./pages/item/item.component";
 import { NosotrosComponent } from "./pages/nosotros/nosotros.component";
 import { ProductosComponent } from "./pages/productos/productos.component";
 
 const app_routes: Router =[
-    { path: '', component: ProductosComponent },
+    { path: '', component: InicioComponent },
     { path: 'nosotros', component: NosotrosComponent},
+    { path: 'productos', component: ProductosComponent },
+    { path: 'contacto', component: ContactoComponent },
+    { path: 'encuentranos', component: EncuentranosComponent },
+    { path: 'eventos', component: EventosComponent },
     { path: 'item', component: ItemComponent },
     { path: '**', pathMatch: 'full', redirectTo: '' },
 ];
